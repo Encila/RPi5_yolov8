ce3f89132fe76af51b47c84958aaae7341a1b2e9
Encila <78430229+Encila@users.noreply.github.com>
2024-05-07 12:20:05 +0200

    fix dimensions tenseur

--- wild_flowers/yolo_flowers_manager.py
+++ wild_flowers/yolo_flowers_manager.py
@@ -67,12 +67,8 @@ class YoloDetectorTFLite:
         else:
             input_img = frame
         input_img = input_img[np.newaxis, ...]  # add batch dim
-        print("DEBUG ndim : ", input_img.ndim)
-        print("DEBUG shape : ", input_img.shape)
         
-        input_img = input_img.astype(np.uint8)  # Ensure data is in UINT8
-        input_img = input_img[np.newaxis, ...]
-        
+        input_img = input_img.astype(np.uint8)  # Ensure data is in UINT8        
         self.interpreter.set_tensor(input_details[0]['index'], input_img)
 
         self.interpreter.invoke()
